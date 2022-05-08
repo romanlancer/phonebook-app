373ceae9f95837f06082f864312f95f6e6798e53
Roman Hyrych <roman.girich@gmail.com>
2022-05-08 21:18:54 +0300

    deleted react-phone-number-input library

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,8 +4,6 @@ import { TiUserAddOutline } from 'react-icons/ti';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import styles from './styles.module.css';
 import PropTypes from 'prop-types';
-import 'react-phone-number-input/style.css';
-import PhoneInput from 'react-phone-number-input';
 
 class Form extends Component {
   state = {
@@ -15,7 +13,7 @@ class Form extends Component {
   };
 
   reset = () => {
-    this.setState({ name: '', number: '', id: '' });
+    this.setState({ name: '', number: '' });
   };
 
   handleSubmit = e => {
@@ -25,15 +23,12 @@ class Form extends Component {
       name: this.state.name,
       number: this.state.number,
     };
-    if (this.state.name.length === 0) return Notify.failure('Enter valid name');
-    if (this.state.number.length !== 13)
-      return Notify.failure('Enter valid number');
     this.props.addContact(contact);
     this.reset();
   };
 
   handleChange = e => {
-    const { name, value } = e.currentTarget;
+    let { name, value } = e.currentTarget;
 
     this.setState({ isDisabled: false, [name]: value });
 
@@ -62,21 +57,21 @@ class Form extends Component {
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             value={this.state.name}
             onChange={e => this.handleChange(e)}
+            required
           />
         </label>
         <label>
           Number:
-          <PhoneInput
+          <input
             type="tel"
-            name="number"
-            defaultCountry="UA"
             placeholder="+380 33 333 3333"
-            initialValueFormat="national"
             className={styles.phoneInputCountry}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             value={this.state.number}
-            onChange={number => this.setState({ number })}
+            name="number"
+            onChange={e => this.handleChange(e)}
+            required
           />
         </label>
 
